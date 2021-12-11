fcdf88bb395dafd2aa75ac82e5a700ad64b81113
vgrugor <vgrugor@gmail.com>
2021-12-11 15:11:50 +0200

    Refactor url API responses

--- app/Http/Controllers/Api/V1/ShortenerController.php
+++ app/Http/Controllers/Api/V1/ShortenerController.php
@@ -8,6 +8,7 @@ use App\Http\Resources\UrlResource;
 use App\Models\Url;
 use App\Services\Shortener\ShortenerDataTransformer;
 use App\Services\Shortener\ShortUrlFactory;
+use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 
@@ -16,10 +17,7 @@ class ShortenerController extends Controller
 
     public function index()
     {
-        return response()->json(
-            UrlResource::collection(Url::all()),
-            Response::HTTP_OK
-        );
+        return response()->json(UrlResource::collection(Url::all()),Response::HTTP_OK);
     }
 
     public function store(UrlShortenerRequest $request, ShortUrlFactory $shortUrlFactory)
@@ -28,10 +26,11 @@ class ShortenerController extends Controller
 
         $shortener = $shortUrlFactory->getShortenerStrategy($dto);
 
-        return response()->json(
-            $shortener->create($dto),
-            Response::HTTP_CREATED
-        );
+        try {
+            return response()->json($shortener->create($dto), Response::HTTP_CREATED);
+        } catch(Exception $e) {
+            return response()->json($e->getMessage(), Response::HTTP_BAD_REQUEST);
+        }
     }
 
     public function show(string $shortKey)
@@ -39,13 +38,15 @@ class ShortenerController extends Controller
         $url = Url::where('short_key', $shortKey)->first();
 
         if ($url !== null) {
-            return new UrlResource($url);
+            return response()->json(new UrlResource($url), Response::HTTP_OK);
         }
+
+        return response()->json(['errors' => 'Not Found'], Response::HTTP_NOT_FOUND);
     }
 
     public function update(Request $request, $id)
     {
-        //TODO: add code
+        //
     }
 
     public function destroy(string $shortKey)
@@ -54,8 +55,9 @@ class ShortenerController extends Controller
 
         if ($url !== null) {
             $url->delete();
+            return response()->json(null, Response::HTTP_NO_CONTENT);
         }
 
-        return response()->json(null, Response::HTTP_NO_CONTENT);
+        return response()->json(['errors' => 'Not Found'], Response::HTTP_BAD_REQUEST);
     }
 }
